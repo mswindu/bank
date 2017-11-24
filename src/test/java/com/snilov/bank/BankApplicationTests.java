72ca84e2873be850e330a07d6dbb52f34d8c68ac
mswindu <windu.whr@gmail.com>
2017-11-24 21:11:42 +0700

    added new tests to verify transactions

--- src/test/java/com/snilov/bank/BankApplicationTests.java
+++ src/test/java/com/snilov/bank/BankApplicationTests.java
@@ -201,6 +201,102 @@ public class BankApplicationTests {
 				.andExpect(jsonPath("$.errors.transactionAmount").value("Transaction amount cannot be empty"));
 	}
 
+	@Test
+	public void testCreateNewTransactionsWithOneAccount() throws Exception {
+		MvcResult result = this.mockMvc.perform(post("/cards")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(createCardsJson("false","1", "DEBIT")))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.blocked").value("false"))
+				.andExpect(jsonPath("$.number").value("1"))
+				.andExpect(jsonPath("$.type").value("DEBIT"))
+				.andReturn();
+
+		String uuidCard = (new JSONObject(result.getResponse().getContentAsString())).getString("uuid");
+
+		this.mockMvc.perform(post("/transactions")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(createTransactionsJson(uuidCard,"DEPOSIT", "10")))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.transactionAmount").value("10"))
+				.andExpect(jsonPath("$.amountBefore").value("0"))
+				.andExpect(jsonPath("$.amountAfter").value("10"));
+
+		this.mockMvc.perform(post("/transactions")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(createTransactionsJson(uuidCard,"DEPOSIT", "100")))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.transactionAmount").value("100"))
+				.andExpect(jsonPath("$.amountBefore").value("10"))
+				.andExpect(jsonPath("$.amountAfter").value("110"));
+
+		this.mockMvc.perform(post("/transactions")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(createTransactionsJson(uuidCard,"WITHDRAW", "5")))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.transactionAmount").value("5"))
+				.andExpect(jsonPath("$.amountBefore").value("110"))
+				.andExpect(jsonPath("$.amountAfter").value("105"));
+	}
+
+	@Test
+	public void testCreateNewTransactionsWithMultipleAccounts() throws Exception {
+		MvcResult result = this.mockMvc.perform(post("/cards")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(createCardsJson("false","1", "DEBIT")))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.blocked").value("false"))
+				.andExpect(jsonPath("$.number").value("1"))
+				.andExpect(jsonPath("$.type").value("DEBIT"))
+				.andReturn();
+
+		String uuidCard1 = (new JSONObject(result.getResponse().getContentAsString())).getString("uuid");
+
+		result = this.mockMvc.perform(post("/cards")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(createCardsJson("false","2", "DEBIT")))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.blocked").value("false"))
+				.andExpect(jsonPath("$.number").value("2"))
+				.andExpect(jsonPath("$.type").value("DEBIT"))
+				.andReturn();
+
+		String uuidCard2 = (new JSONObject(result.getResponse().getContentAsString())).getString("uuid");
+
+		this.mockMvc.perform(post("/transactions")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(createTransactionsJson(uuidCard1,"DEPOSIT", "10")))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.transactionAmount").value("10"))
+				.andExpect(jsonPath("$.amountBefore").value("0"))
+				.andExpect(jsonPath("$.amountAfter").value("10"));
+
+		this.mockMvc.perform(post("/transactions")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(createTransactionsJson(uuidCard2,"DEPOSIT", "100")))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.transactionAmount").value("100"))
+				.andExpect(jsonPath("$.amountBefore").value("0"))
+				.andExpect(jsonPath("$.amountAfter").value("100"));
+
+		this.mockMvc.perform(post("/transactions")
+				.contentType(MediaType.APPLICATION_JSON_UTF8)
+				.content(createTransactionsJson(uuidCard1,"WITHDRAW", "5")))
+				.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.transactionAmount").value("5"))
+				.andExpect(jsonPath("$.amountBefore").value("10"))
+				.andExpect(jsonPath("$.amountAfter").value("5"));
+	}
+
 
 	private static String createAccountJson(String currency, String balance, String type) {
 		return "{ \"currency\": \"" + currency + "\", " +
