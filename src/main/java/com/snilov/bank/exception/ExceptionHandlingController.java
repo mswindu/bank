e507846cceae0f17f3b6b0f4d57d8021d29190e3
mswindu <windu.whr@gmail.com>
2017-11-26 19:34:13 +0700

    refactoring application

--- src/main/java/com/snilov/bank/exception/ExceptionHandlingController.java
+++ src/main/java/com/snilov/bank/exception/ExceptionHandlingController.java
@@ -1,11 +1,16 @@
 package com.snilov.bank.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionHandlingController extends ResponseEntityExceptionHandler {
@@ -22,6 +27,19 @@ public class ExceptionHandlingController extends ResponseEntityExceptionHandler 
         }
 
         return ResponseEntity.noContent().build();
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+                                                                         HttpHeaders headers, HttpStatus status, WebRequest request) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(ex.getMethod());
+        builder.append(" method is not supported for this request. Supported methods are ");
+        ex.getSupportedHttpMethods().forEach(t -> builder.append(t).append(" "));
+
+        Map<String, String> errors = new HashMap<>();
+        errors.put("description", builder.toString());
+        return new ResponseEntity<>(new ResponseException(ex.getMessage(), errors), HttpStatus.METHOD_NOT_ALLOWED);
     }
 
 }
