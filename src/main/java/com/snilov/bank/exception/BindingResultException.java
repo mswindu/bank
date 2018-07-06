d0db0f1cba2a93f29c816bbaaf55745055e8155c
mswindu <windu.whr@gmail.com>
2018-07-06 11:48:21 +0700

    Обновление версий + интеграция

--- src/main/java/com/snilov/bank/exception/BindingResultException.java
+++ src/main/java/com/snilov/bank/exception/BindingResultException.java
@@ -1,6 +1,7 @@
 package com.snilov.bank.exception;
 
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Exception thrown if there were any validation errors.
@@ -15,5 +16,15 @@ public class BindingResultException extends RuntimeException {
 
     public Map<String, String> getErrors() {
         return errors;
+    }
+
+    @Override
+    public String getMessage() {
+        Set keys = errors.keySet();
+        StringBuilder result = new StringBuilder("Error parameters: ");
+        for (Object key: keys) {
+            result.append(key.toString()).append(", ");
+        }
+        return String.valueOf(result);
     }
 }
